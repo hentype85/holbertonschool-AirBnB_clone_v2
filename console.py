145a53de202462626306f84f9aff857f05b536a3
Martin <martinleiro9@gmail.com>
2023-07-31 13:08:15 -0700

    fix pycodestyle

--- console.py
+++ console.py
@@ -73,8 +73,8 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] == '{' and pline[-1] == '}'\
-                        and type(eval(pline)) == dict:
+                    if pline[0] == '{' and \
+                            pline[-1] == '}' and type(eval(pline)) == dict:
                         _args = pline
                     else:
                         _args = pline.replace(',', '')
@@ -138,7 +138,7 @@ class HBNBCommand(cmd.Cmd):
                 v = v.replace("_", " ")
 
         new_instance = self.classes[class_name](**dictionary)
-        storage.save()    
+        storage.save()
         print(new_instance.id)
         storage.save()
 
@@ -203,7 +203,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            del (storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -335,6 +335,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
