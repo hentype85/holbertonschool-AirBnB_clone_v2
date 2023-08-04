011966ee10aa75a953356bce607c6d428df403ea
Martin <martinleiro9@gmail.com>
2023-08-04 09:08:48 -0700

    9

--- models/place.py
+++ models/place.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.city import City
+from models.user import User
 from sqlalchemy.orm import relationship
 from sqlalchemy import ForeignKey
 from sqlalchemy import Column, String, Integer, Float
+import models
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    city_id = Column(String(60), ForeignKey(City.id), nullable=False)
+    user_id = Column(String(60), ForeignKey(User.id), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024))
     number_rooms = Column(Integer, default=0)
@@ -19,3 +22,14 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0)
     latitude = Column(Float)
     longitude = Column(Float)
+
+
+    @property
+    def reviews(self):
+        """ returns the list of Review instances
+            with place_id equals to the current Place.id"""
+        list_review = []
+        for review in models.storage.all(models.Review).values():
+            if review.place_id == self.id:
+                list_review.append(review)
+        return list_review
