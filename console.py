19370742165f42111f4e83e9aa33735d4a2e0113
Martin <some@gmail.com>
2023-08-04 09:31:16 -0700

    fix

--- console.py
+++ console.py
@@ -13,7 +13,7 @@ from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
-    """ Contains the functionality for the HBNB console"""
+    """functionality for the HBNB console"""
 
     # determines prompt for interactive/non-interactive modes
     prompt = '(hbnb) '
