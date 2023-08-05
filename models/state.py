68a0d4d4a993d5443ed526b98f6b66501fb2f481
Martin <some@gmail.com>
2023-08-04 19:18:28 -0700

    state class update

--- models/state.py
+++ models/state.py
@@ -13,13 +13,16 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state")
 
-    @property
-    def cities(self):
-        from models.city import City
-        """ returns the list of City instances with state_id 
-            equals to the current State.id"""
-        list_city = []
-        for city in models.storage.all(City).values():
-            if city.state.id == self.id:
-                list_city.append(city)
-        return list_city
+    if getenv("HBNB_TYPE_STORAGE", None) != "db":
+        """for FileStorage"""
+
+        @property
+        def cities(self):
+            from models.city import City
+            """ returns the list of City instances with state_id 
+                equals to the current State.id"""
+            list_city = []
+            for city in models.storage.all(City).values():
+                if city.state.id == self.id:
+                    list_city.append(city)
+            return list_city
