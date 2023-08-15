b6cd32457e25066106a93ef41a7f5f17565ebfa7
Martin <some@gmail.com>
2023-08-15 06:52:53 -0700

    add route task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application
+    python3 -m web_flask.2-c_route
+    curl 0.0.0.0:5000/c/is_fun ; echo "" | cat -e
+    curl 0.0.0.0:5000/c/cool ; echo "" | cat -e
+    curl 0.0.0.0:5000/c
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_route():
+    """home"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def index():
+    """route /hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def Cisfun(text):
+    """route /C/"""
+    return "C {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
